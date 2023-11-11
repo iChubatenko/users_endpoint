4455d462769d795d5d31c43dbd10024ea0d4011e
ichubatenko <doubleredbull@gmail.com>
2023-11-11 17:05:29 +0100

    Second commit: added tests for Controller and Repository

---
+++ src/test/java/com/ihorchubatenko/spring/web/app/controller/UsersControllerTest.java
@@ -0,0 +1,88 @@
+package com.ihorchubatenko.spring.web.app.controller;
+
+import com.ihorchubatenko.spring.web.app.dao.User;
+import com.ihorchubatenko.spring.web.app.dao.UserRepository;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+
+public class UsersControllerTest extends TestCase {
+
+    @Test
+    public void testGetUserByID() {
+        User user = new User(1, "Ihor", "Chubatenko");
+
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        Mockito.when(mockRepo.getUserById(1)).thenReturn(user);
+
+        UsersController testedInstance = new UsersController(mockRepo);
+
+        ResponseEntity<User> userByID = testedInstance.getUserByID(1L);
+
+        assertEquals(user, userByID.getBody());
+    }
+
+    @Test
+    public void testGetAllUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Ihor", "Chubatenko"));
+        users.add(new User(2, "Serhii", "Zapalskiy"));
+
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        Mockito.when(mockRepo.getAllUsers()).thenReturn(users);
+
+        UsersController testedInstance = new UsersController(mockRepo);
+
+        ResponseEntity<List<User>> allUsers = testedInstance.getAllUsers();
+
+        assertEquals(users, allUsers.getBody());
+
+    }
+
+    @Test
+    public void testCreateUser() {
+        User testUser = new User(1, "Ihor", "Chubatenko");
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        Mockito.when(mockRepo.saveUser(any(User.class))).thenReturn(testUser);
+
+        UsersController testedInstance = new UsersController(mockRepo);
+
+        ResponseEntity<User> savedUser = testedInstance.createUser(testUser);
+
+        assertEquals(testUser, savedUser.getBody());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        User testUser = new User(1, "Ihor", "Chubatenko");
+
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        Mockito.when(mockRepo.getUserById(1L)).thenReturn(testUser);
+        Mockito.when(mockRepo.saveUser(any(User.class))).thenReturn(testUser);
+        Mockito.when(mockRepo.getUserById(2L)).thenReturn(null);
+
+        UsersController testedInstance = new UsersController(mockRepo);
+
+        ResponseEntity<User> savedUser = testedInstance.updateUser(1L, testUser);
+
+        assertEquals(testUser, savedUser.getBody());
+
+    }
+
+    @Test
+    public void testDeleteUser() {
+        User testUser = new User(1, "Ihor", "Chubatenko");
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        Mockito.when(mockRepo.getUserById(1L)).thenReturn(testUser);
+        mockRepo.deleteUser(1L);
+        Mockito.when(mockRepo.getUserById(2L)).thenReturn(null);
+
+        Mockito.verify(mockRepo, Mockito.times(1)).deleteUser(1L);
+    }
+}
